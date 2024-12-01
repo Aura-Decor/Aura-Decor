0e324af0584438a14743dde6b2204a63688d2728
Mo7ammedd <medo.mostafa22255@gmail.com>
2024-12-01 14:00:46 +0200

    added redis connection string

--- AuraDecor.Repository/Data/AppDbContext.cs
+++ AuraDecor.Repository/Data/AppDbContext.cs
@@ -1,25 +1,24 @@
-﻿    using System.Reflection;
-    using AuraDecor.Core.Entities;
-    using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
-    using Microsoft.EntityFrameworkCore;
+﻿using System.Reflection;
+using AuraDecor.Core.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 
-    namespace AuraDecor.Repository.Data;
+namespace AuraDecor.Repository.Data;
 
-    public class AppDbContext : IdentityDbContext<User>
+public class AppDbContext : IdentityDbContext<User>
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
     {
-        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
-        {
-        }
-
-        // protected override void OnModelCreating(ModelBuilder modelBuilder)
-        // {
-        //
-        //     modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
-        // }
-        //
-        public DbSet<Furniture> Furniture { get; set; }
-        
-        public DbSet<FurnitureCategory> FurnitureCategories { get; set; }
-        
-        public DbSet<FurnitureBrand> FurnitureBrands { get; set; }
     }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder); 
+
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+    }
+
+    public DbSet<Furniture> Furniture { get; set; }
+    public DbSet<FurnitureCategory> FurnitureCategories { get; set; }
+    public DbSet<FurnitureBrand> FurnitureBrands { get; set; }
+}
