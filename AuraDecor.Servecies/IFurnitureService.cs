3b6c7e4beefe4246c38deefbc0308162e78164d3
Hasnaa <a02040456@gmail.com>
2025-02-13 19:09:29 +0200

    add CRUD Furniture

---
+++ AuraDecor.Servecies/IFurnitureService.cs
@@ -0,0 +1,21 @@
+﻿using AuraDecor.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuraDecor.Servicies
+{
+    public interface IFurnitureService
+    {
+        Task<Furniture> GetFurnitureByIdAsync(Guid id);
+        Task<IReadOnlyList<Furniture>> GetAllFurnitureAsync();
+
+        Task AddFurnitureAsync(Furniture furniture);
+        Task UpdateFurnitureAsync(Furniture furniture);
+        Task DeleteFurnitureAsync(Furniture furniture);
+
+
+    }
+}
