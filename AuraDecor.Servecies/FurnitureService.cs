40b954772347440edc174e181f3295f68c11cc09
Hasnaa <a02040456@gmail.com>
2025-02-13 19:09:29 +0200

    add CRUD Furniture

---
+++ AuraDecor.Servecies/FurnitureService.cs
@@ -0,0 +1,49 @@
+﻿using AuraDecor.Core.Entities;
+using AuraDecor.Core.Repositories.Contract;
+using AuraDecor.Repositoriy.Migrations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuraDecor.Servicies
+{
+    public class FurnitureService : IFurnitureService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public FurnitureService(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task AddFurnitureAsync(Furniture furniture)
+        {
+            _unitOfWork.Repository<Furniture>().Add(furniture);
+            await _unitOfWork.CompleteAsync();
+        }
+
+        public async Task DeleteFurnitureAsync(Furniture furniture)
+        {
+             _unitOfWork.Repository<Furniture>().DeleteAsync(furniture);
+            await _unitOfWork.CompleteAsync();
+        }
+
+        public async Task<IReadOnlyList<Furniture>> GetAllFurnitureAsync()
+        {
+            return await _unitOfWork.Repository<Furniture>().GetAllAsync();
+        }
+
+        public async Task<Furniture> GetFurnitureByIdAsync(Guid id)
+        {
+            return await _unitOfWork.Repository<Furniture>().GetByIdAsync(id);
+        }
+
+        public async Task UpdateFurnitureAsync(Furniture furniture)
+        {
+            _unitOfWork.Repository<Furniture>().UpdateAsync(furniture);
+            await _unitOfWork.CompleteAsync();
+        }
+    }
+}
