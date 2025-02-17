2d64580000e0b19f3f88a5e883e1831e2cb28ba2
mo7ammedd <medo.mostafa22255@gmail.com>
2025-02-17 03:14:25 +0200

    add AdminController

--- AuraDecor.APIs/Program.cs
+++ AuraDecor.APIs/Program.cs
@@ -21,10 +21,8 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 app.UseMiddleware<ExceptionMiddleware>();
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwaggerMiddleWare();
-}
+app.UseSwaggerMiddleWare(); // Always enable Swagger
+
 app.UseStatusCodePagesWithRedirects("/errors/{0}");
 app.UseHttpsRedirection();
 app.UseStaticFiles();
