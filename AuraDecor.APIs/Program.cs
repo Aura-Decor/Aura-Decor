15dbaa1c4ea9d8cd2fec6b5022a4e32bd850398b
mo7ammedd <medo.mostafa22255@gmail.com>
2025-02-21 03:31:56 +0200

    add Specifications

--- AuraDecor.APIs/Program.cs
+++ AuraDecor.APIs/Program.cs
@@ -1,10 +1,11 @@
 #region Using Directives
 using AuraDecor.APIs.Extensions;
 using AuraDecor.APIs.Middlewares;
+using AuraDecor.Repository.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Scalar.AspNetCore;
-
-
-
+using Microsoft.AspNetCore.Cors; 
 #endregion
 
 #region Builder Configuration
@@ -14,10 +15,41 @@ builder.Services.AddControllers();
 builder.Services.AddApplicationServices(builder.Configuration);
 builder.Services.AddIdentityServices(builder.Configuration);
 builder.Services.AddSwaggerServices();
+
+// Add CORS services
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", builder =>
+    {
+        builder.AllowAnyOrigin()
+            .AllowAnyMethod()
+            .AllowAnyHeader();
+    });
+});
 #endregion
 
 #region Application Configuration
+
 var app = builder.Build();
+#region DatabaseMigration
+using var scope = app.Services.CreateScope();
+          
+// Ask CLR to create a scope for the service provider
+var services = scope.ServiceProvider;
+var _dbcontext = services.GetRequiredService<AppDbContext>();
+var loggerFactory = services.GetRequiredService<ILoggerFactory>();
+try
+{
+    await _dbcontext.Database.MigrateAsync();
+    await AppDbContextDataSeed.SeedAsync(_dbcontext);
+                
+}
+catch (Exception e)
+{
+    var logger = loggerFactory.CreateLogger<Program>();
+    logger.LogError(e, "An error occurred during migration");
+}
+#endregion
 
 app.UseMiddleware<ExceptionMiddleware>();
 app.UseSwaggerMiddleWare(); 
@@ -30,6 +62,7 @@ app.MapScalarApiReference(options =>
 app.UseStatusCodePagesWithRedirects("/errors/{0}");
 app.UseHttpsRedirection();
 app.UseStaticFiles();
+app.UseCors("AllowAll"); 
 app.UseAuthentication();
 app.UseAuthorization();
 app.MapControllers();
