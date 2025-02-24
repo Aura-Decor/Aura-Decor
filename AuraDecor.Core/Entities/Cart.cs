3f2c5523add71a1fc16e6ad923fd55e71f8782d5
Hasnaa <a02040456@gmail.com>
2025-02-24 05:02:36 +0200

    add cart

---
+++ AuraDecor.Core/Entities/Cart.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuraDecor.Core.Entities
+{
+    public class Cart : BaseEntity
+    {
+        public Guid UserId { get; set; }
+        public ICollection<CartItem> cartItems { get; set; } = new List<CartItem>();
+    }
+}
