8eda34ad501ba2cffea5b50ff6f9da169349ee4d
Hasnaa <a02040456@gmail.com>
2025-03-06 18:37:41 +0200

    Cart Services

--- AuraDecor.Services/CartService.cs
+++ AuraDecor.Services/CartService.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using AuraDecor.Core.Entities;
+using AuraDecor.Core.Repositories.Contract;
+using AuraDecor.Core.Services.Contract;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,32 @@ using System.Threading.Tasks;
 
 namespace AuraDecor.Servicies
 {
-    public class CartService
+    public class CartService : ICartService
     {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public CartService(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task AddToCartAsync(CartItem cart)
+        {
+            _unitOfWork.Repository<CartItem>().Add(cart);
+            await _unitOfWork.CompleteAsync();
+
+        }
+
+        public Task<Cart> GetCartByUserIdAsync(Guid Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task RemoveFromCartAsync(CartItem cart)
+        {
+            _unitOfWork.Repository<CartItem>().DeleteAsync(cart);
+            await _unitOfWork.CompleteAsync();
+
+        }
     }
 }
