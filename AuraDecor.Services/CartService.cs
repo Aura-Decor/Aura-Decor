a3a6e25bdc665d745473e232cb61d602b5777429
Hasnaa <a02040456@gmail.com>
2025-03-15 01:52:41 +0200

    add cart specifications

--- AuraDecor.Services/CartService.cs
+++ AuraDecor.Services/CartService.cs
@@ -1,11 +1,9 @@
 ﻿using AuraDecor.Core.Entities;
 using AuraDecor.Core.Repositories.Contract;
 using AuraDecor.Core.Services.Contract;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using  AuraDecor.Core.Specifications.CartSpecification;
+//using AuraDecor.APIs.Errors;
+
 
 namespace AuraDecor.Servicies
 {
@@ -18,23 +16,74 @@ namespace AuraDecor.Servicies
             _unitOfWork = unitOfWork;
         }
 
-        public async Task AddToCartAsync(CartItem cart)
+        public async Task AddToCartAsync(string userId, Guid furnitureId, int quantity)
         {
-            _unitOfWork.Repository<CartItem>().Add(cart);
+            var Cartspec = new CartByUserIdSpecification(userId);
+            var cart = await _unitOfWork.Repository<Cart>().GetWithSpecAsync(Cartspec);
+            if (cart == null)
+            {
+                cart = new Cart { UserId = userId };
+                 _unitOfWork.Repository<Cart>().Add(cart);
+                await _unitOfWork.CompleteAsync();
+            }
+            var spec = new CartItemSpecification(cart.Id, furnitureId);
+            var existingItem = await _unitOfWork.Repository<CartItem>().GetWithSpecAsync(spec);
+
+
+            if (existingItem != null)
+            {
+                existingItem.Quantity += quantity;
+                _unitOfWork.Repository<CartItem>().UpdateAsync(existingItem);
+            }
+            else
+            {
+                var furniture = await _unitOfWork.Repository<Furniture>().GetByIdAsync(furnitureId);
+                if (furniture == null)
+                {
+                 //   return new ApiResponse(404, "Furniture item not found.");
+                }
+
+                var cartItem = new CartItem
+                {
+                    CartId = cart.Id,
+                    FurnitureId = furnitureId,
+                    Quantity = quantity
+                };
+
+                 _unitOfWork.Repository<CartItem>().Add(cartItem);
+            }
+
             await _unitOfWork.CompleteAsync();
+          //  return new ApiResponse(200, "Item added to cart successfully.");
+
 
         }
 
-        public Task<Cart> GetCartByUserIdAsync(Guid Id)
+        public async Task<Cart> GetCartByUserIdAsync(string userId)
         {
-            throw new NotImplementedException();
+            var spec = new CartItemsWithSpecification(userId);
+            return await _unitOfWork.Repository<Cart>().GetWithSpecAsync(spec);
+
         }
 
-        public async Task RemoveFromCartAsync(CartItem cart)
+        public async Task RemoveFromCartAsync(string userId, Guid furnitureId)
         {
-            _unitOfWork.Repository<CartItem>().DeleteAsync(cart);
-            await _unitOfWork.CompleteAsync();
 
+            var spec = new CartByUserIdSpecification(userId);
+            var cart = await _unitOfWork.Repository<Cart>().GetWithSpecAsync(spec);
+
+            if (cart == null)
+                throw new Exception("Cart not found");
+
+            var specItem = new CartItemSpecification(cart.Id, furnitureId);
+            var cartItem = await _unitOfWork.Repository<CartItem>().GetWithSpecAsync(specItem);
+
+            if (cartItem == null)
+                throw new Exception("Item not found in cart");
+
+            _unitOfWork.Repository<CartItem>().DeleteAsync(cartItem);
+
+            await _unitOfWork.CompleteAsync();
         }
     }
 }
